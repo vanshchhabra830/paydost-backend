485ba1a81bcbbabf380ef238d4d6cff91af11362
vanshchhabra830 <vanshchhabra830@gmail.com>
2026-08-09 03:41:29 +0530

    chore: secure secrets using .env and environment placeholders

--- src/main/java/com/example/paydost/PayDostApplication.java
+++ src/main/java/com/example/paydost/PayDostApplication.java
@@ -10,3 +10,4 @@ public class PayDostApplication {
         SpringApplication.run(PayDostApplication.class, args);
     }
 }
+
--- src/main/java/com/example/paydost/config/RedisConfig.java
+++ src/main/java/com/example/paydost/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.example.paydost.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -32,12 +35,18 @@ public class RedisConfig {
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(serializer);
         template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(serializer);
         template.afterPropertiesSet();
         return template;
     }
